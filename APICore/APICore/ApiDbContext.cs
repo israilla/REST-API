99091e6fd8444d22ad4b23286ff9a28624bf894e
Israilla <israillacaroline@hotmail.com>
2020-08-29 17:13:32 -0300

    Created API Core

---
+++ APICore/APICore/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APICore.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICore
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
---
+++ APICore/APICore/Controllers/FornecedoresController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APICore;
+using APICore.Model;
+
+namespace APICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FornecedoresController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public FornecedoresController(ApiDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Fornecedor>>> GetFornecedores()
+        {
+            return await _context.Fornecedores.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Fornecedor>> GetFornecedor(Guid id)
+        {
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return fornecedor;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFornecedor(Guid id, Fornecedor fornecedor)
+        {
+            if (id != fornecedor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fornecedor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FornecedorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            //204
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Fornecedor>> PostFornecedor(Fornecedor fornecedor)
+        {
+            _context.Fornecedores.Add(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFornecedor", new { id = fornecedor.Id }, fornecedor);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Fornecedor>> DeleteFornecedor(Guid id)
+        {
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fornecedores.Remove(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return fornecedor;
+        }
+
+        private bool FornecedorExists(Guid id)
+        {
+            return _context.Fornecedores.Any(e => e.Id == id);
+        }
+    }
+}
